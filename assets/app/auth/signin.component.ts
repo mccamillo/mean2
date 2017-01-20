303d11f62b3a8468401872b7b7b1158b7cc76650
Mauricio <mauricio.camillo@matera.com>
2017-01-20 14:25:33 -0200

    Messages

--- assets/app/auth/signin.component.ts
+++ assets/app/auth/signin.component.ts
@@ -1,15 +1,14 @@
-import { Component } from "@angular/core";
+import {Component, OnInit} from "@angular/core";
 import { FormGroup, FormControl, Validators } from "@angular/forms";
 
 @Component({
     selector: 'app-signin',
     templateUrl: './signin.component.html'
 })
-export class SigninComponent {
+export class SigninComponent implements OnInit{
     myForm: FormGroup;
 
     onSubmit() {
-        console.log(this.myForm);
         this.myForm.reset();
     }
 
